32e2da78ff2792d357b70846cf20b276d3e6df41
SyntaxEvgen <syntax-evgen@yandex.ru>
2023-01-21 20:09:55 +0300

    Добавьте файлы проекта.

---
+++ LibraryService.Web/Controllers/LibraryController.cs
@@ -0,0 +1,29 @@
+﻿using LibraryService.Web.Models;
+using LibraryServiceReference;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LibraryService.Web.Controllers
+{
+    public class LibraryController : Controller
+    {
+        public IActionResult Index(SearchType searchType, string searchString)
+        {
+
+            LibraryWebServiceSoapClient client =
+                new LibraryWebServiceSoapClient(LibraryWebServiceSoapClient.EndpointConfiguration.LibraryWebServiceSoap);
+
+            if (!string.IsNullOrEmpty(searchString) && searchString.Length >= 3)
+                switch (searchType)
+                {
+                    case SearchType.Title:
+                        return View(new BookCategoryViewModel() { Books = client.GetBooksByTitle(searchString) });
+                    case SearchType.Category:
+                        return View(new BookCategoryViewModel() { Books = client.GetBooksByCategory(searchString) });
+                    case SearchType.Author:
+                        return View(new BookCategoryViewModel() { Books = client.GetBooksByAuthor(searchString) });
+                }
+
+            return View(new BookCategoryViewModel() { Books = new Book[] { } });
+        }
+    }
+}
